43d4212d53e7cbdf4d6da3266c93acc1493aecf1
stephen-harlow <srh150030@utdallas.edu>
2016-10-03 15:14:14 -0500

    It's (kinda) ALIVE

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-
+// sg>xhpQ?ZJpFA[q4K
+// ACCESS_TOKEN: EAADJaV9WwikBAESTqOHziFZCJyvktOlOKNAT8N6V7K9hQBPkYpLZALgadBx3AYK1wHoR1PYoVn7chU2WCD8dGzRMHdxlAolbrzypBLrZCs1fV6XRHCkiCqz7M2PZBVxUHjznpEhPMZA6b5lDpkDwer6VFZAYjghpZBfRmHf0GTsiQZDZD
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -25,7 +26,38 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
 
+const token = process.env.ACCESS_TOKEN
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
