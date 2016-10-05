582a5250c9d045a63d1c3bfc4990ca05a6d24761
stephen-harlow <srh150030@utdallas.edu>
2016-10-04 22:05:20 -0500

    Fix Output

--- index.js
+++ index.js
@@ -147,35 +147,35 @@ function sendGenericMessage(sender, results) {
     var first_item = results[0];
     var ez = []
     ez.push(GenMainCard(first_item.mediaEntity))
-
+    ez.push.apply({
+       "title": "First card",
+       "subtitle": "Element #1 of an hscroll",
+       "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+       "buttons": [{
+           "type": "web_url",
+           "url": "https://www.messenger.com",
+           "title": "web url"
+       }, {
+           "type": "postback",
+           "title": "Postback",
+           "payload": "Payload for first element in a generic bubble",
+       }],
+   }, {
+       "title": "Second card",
+       "subtitle": "Element #2 of an hscroll",
+       "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+       "buttons": [{
+           "type": "postback",
+           "title": "Postback",
+           "payload": "Payload for second element in a generic bubble",
+       }],
+   })
     let messageData = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
-                "elements":[{
-                   "title": "First card",
-                   "subtitle": "Element #1 of an hscroll",
-                   "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                   "buttons": [{
-                       "type": "web_url",
-                       "url": "https://www.messenger.com",
-                       "title": "web url"
-                   }, {
-                       "type": "postback",
-                       "title": "Postback",
-                       "payload": "Payload for first element in a generic bubble",
-                   }],
-               }, {
-                   "title": "Second card",
-                   "subtitle": "Element #2 of an hscroll",
-                   "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                   "buttons": [{
-                       "type": "postback",
-                       "title": "Postback",
-                       "payload": "Payload for second element in a generic bubble",
-                   }],
-               }]
+                "elements":ez
             }
         }
     }
