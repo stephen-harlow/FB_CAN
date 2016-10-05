cebe117b5c39dbf670b204586e13ecd687c86a4e
stephen-harlow <srh150030@utdallas.edu>
2016-10-04 21:30:16 -0500

    Try default Templates

--- index.js
+++ index.js
@@ -154,7 +154,29 @@ function sendGenericMessage(sender, results) {
             "type": "template",
             "payload": {
                 "template_type": "generic",
-                "elements": ez
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
             }
         }
     }
